bce9930fe2ade18c5b5b8b61bf82352ce3a7887d
Oneted11 <muriithited@gmail.com>
2022-05-19 11:25:34 +0300

    creating componengs and passing props

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
+//components
+function P1(props) {
+  return <h1 style={{ color: "green" }}>{props.text}</h1>;
+}
+function P2(props) {
+  return <h1 style={{ color: "red" }}> {props.text}</h1>;
+}
+
+//composing components into page
 ReactDOM.render(
   <article>
-    <h1>HEllo there </h1>
-    <h1 style={{ color: "red" }}> GeneRAl KenoBI</h1>
+    <P1 text="heLLO tHERe" />
+    <P2 text="gENErAL keNObi" />
   </article>,
   document.getElementById("root")
 );
